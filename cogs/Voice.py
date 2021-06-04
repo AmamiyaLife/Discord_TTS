8dbe6d7b71201d828b7498ceed2e0d434c94b736
AmamiyaLife <85048698+AmamiyaLife@users.noreply.github.com>
2021-06-04 18:50:03 +0900

    Update Voice.py

--- cogs/Voice.py
+++ cogs/Voice.py
@@ -1,1 +1,23 @@
+from discord.ext import commands # Bot Commands Frameworkのインポート
 
+import discord # discord.pyをインポート
+
+# コグとして用いるクラスを定義。
+class TestCog(commands.Cog):
+
+    # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+    def __init__(self, bot):
+        self.bot = bot
+
+    # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong!')
+
+    @commands.command()
+    async def what(self, ctx, what):
+        await ctx.send(f'{what}とはなんですか？')
+
+# Bot本体側からコグを読み込む際に呼び出される関数。
+def setup(bot):
+    bot.add_cog(TestCog(bot)) # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
